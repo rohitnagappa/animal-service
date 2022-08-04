98e44daf962345c28ed935389ac59b95438a5f9e
Rohit Nagappa <108780630+rohitnagappa@users.noreply.github.com>
2022-08-04 10:21:28 +0530

    section C-2

--- src/main/java/org/nagappa/domain/models/Butterfly.java
+++ src/main/java/org/nagappa/domain/models/Butterfly.java
@@ -3,27 +3,52 @@ package org.nagappa.domain.models;
 import org.nagappa.domain.ifc.Animal;
 
 public class Butterfly implements Animal {
+
+    private boolean isCatP;
+
+    Butterfly(){
+    }
+    Butterfly(boolean isCatP){
+        this.isCatP = isCatP;
+    }
+
     @Override
     public boolean fly() {
-        System.out.println("As i am butterfly, i can fly");
-        return true;
+        if(isCatP){
+            System.out.println("As i am catterpillar, i cannot fly");
+        }else {
+            System.out.println("As i am butterfly, i can fly");
+        }
+        return !isCatP;
     }
 
     @Override
     public boolean walk() {
-        System.out.println("As i am butterfly, i cannot walk");
-        return false;
+        if(isCatP){
+            System.out.println("As i am catterpillar, i can walk/crawl");
+        }else {
+            System.out.println("As i am butterfly, i cannot walk");
+        }
+        return isCatP;
     }
 
     @Override
     public boolean sing() {
-        System.out.println("As i am butterfly, i cannot sing");
+        System.out.println("i cannot sing");
         return false;
     }
 
     @Override
     public boolean swim() {
-        System.out.println("As i am butterfly, i cannot swim.");
+        System.out.println("i cannot swim.");
         return false;
+    }
+
+    public boolean isCatP() {
+        return isCatP;
+    }
+
+    public void setCatP(boolean catP) {
+        isCatP = catP;
     }
 }
--- src/test/java/org/nagappa/domain/models/ButterflyTest.java
+++ src/test/java/org/nagappa/domain/models/ButterflyTest.java
@@ -9,28 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class ButterflyTest {
 
     private Animal butterfly;
+
+    private Animal caterpillar;
     @BeforeEach
     void setUp() {
         butterfly = new Butterfly();
+        caterpillar = new Butterfly(true);
     }
 
     @Test
     void fly() {
         assertTrue(butterfly.fly());
+        assertFalse(caterpillar.fly());
     }
 
     @Test
     void walk() {
         assertFalse(butterfly.walk());
+        assertTrue(caterpillar.walk());
     }
 
     @Test
     void sing() {
         assertFalse(butterfly.sing());
+        assertFalse(caterpillar.sing());
     }
 
     @Test
     void swim() {
         assertFalse(butterfly.swim());
+        assertFalse(caterpillar.swim());
     }
 }
